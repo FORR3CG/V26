39c17bf3c931b7bd64cde1bd87b51ce90351aee9
gestskoli <ges@tskoli.is>
2026-03-12 10:09:04 +0000

    Add files via upload

---
+++ Tímar/kafli_3/src/main.rs
@@ -0,0 +1,22 @@
+const KLUKKUSTUNDIR_I_SOLARHRING: u8 = 24;
+static ABC: i32 = 100;
+
+fn main() {
+    let y: i128 = 10;
+    let mut x= 10_i128;
+    x = x + 1;
+    let klst = KLUKKUSTUNDIR_I_SOLARHRING;
+    let a = ABC;
+    let nafn = "Geir";
+    let nafn: i32 = 10;
+
+    // i8/u8, i16/u16, i32/u32, i64/u64, i128/u128, isize/usize
+    // i == signed -(2^(n-1)) upp í 2^(n-1)-1
+    // u == unsigned 0 upp í 2^n - 1
+    let x: u8 = 255;
+    //let y: i8 = 255; 
+    let k = 123_i16;
+    let j = 0o555;
+    let idx: usize = 23;
+
+}
