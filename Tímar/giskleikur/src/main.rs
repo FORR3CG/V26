7b48f2905957cfb46adbbede8689fc381515f2eb
gestskoli <ges@tskoli.is>
2026-03-12 09:15:12 +0000

    Update main.rs

--- Tímar/giskleikur/src/main.rs
+++ Tímar/giskleikur/src/main.rs
@@ -1,13 +1,17 @@
-use std::io;
+use std::{cmp::Ordering, io};
+use rand::RngExt;
 
 fn main() {
     println!("Giskaðu á töluna!");
 
+    let leynitala = rand::rng().random_range(1..=100);
     loop {
         println!("Giskaðu núna: ");
         let mut inntak = String::new(); // býr tóman streng
         println!("stærð: {}, pláss: {}", inntak.len(), inntak.capacity()); // 0 og 0
-        io::stdin().read_line(&mut inntak).expect("Gat ekki lesið frá notanda!!");
+        io::stdin()
+            .read_line(&mut inntak)
+            .expect("Gat ekki lesið frá notanda!!");
         
         let gisktala: u8 = match inntak.trim().parse() {
             Ok(tala) => tala,
@@ -16,6 +20,15 @@ fn main() {
                 continue;
             }
         };
+        match gisktala.cmp(&leynitala) {
+            Ordering::Less => println!("{} er of lág!", gisktala),
+            Ordering::Greater => println!("{} er of há!", gisktala),
+            Ordering::Equal => {
+                println!("{} er rétt!!!!!", gisktala);
+                break;
+            }
+        }
+
 
         println!("Þú slóst inn {}", gisktala);
     }
